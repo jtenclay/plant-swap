cd296087f591b8451ffa35a1c994537608952f68
Jackson Tenclay <jptenclay@gmail.com>
2017-07-24 10:55:26 -0500

    backend routes for users, tags, comments, swaps

--- client/plant-swap/src/app/app.module.ts
+++ client/plant-swap/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
