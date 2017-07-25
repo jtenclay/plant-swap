5ec90ffb26e4119c6fe3e2100b6593e9110b3f37
Jackson Tenclay <jptenclay@gmail.com>
2017-07-25 16:41:47 -0500

    post swap but no update

--- client/plant-swap/src/app/account/account.component.ts
+++ client/plant-swap/src/app/account/account.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Http, Response } from '@angular/http';
 import { DataService } from '../data.service';
+
+class Swap {
+	id: number;
+	user_id: number;
+	title: string;
+	description: string;
+	created_at: string;
+	is_open: boolean;
+	coswap_id: number;
+	image_url: string;
+}
 
 @Component({
   selector: 'app-account',
@@ -9,7 +21,13 @@ import { DataService } from '../data.service';
 })
 export class AccountComponent {
 
-  constructor(private router: Router, private dataService: DataService) { }
+	showNewModal: boolean = false;
+	newSwap: Swap = new Swap();
+
+  constructor(private router: Router, private dataService: DataService, private http: Http) {
+  	this.newSwap.user_id = window.localStorage.id;
+  	this.newSwap.is_open = true;
+  }
 
   logout($event) {
   	$event.preventDefault();
@@ -19,6 +37,17 @@ export class AccountComponent {
   	this.router.navigate(['/']);
   }
 
+  toggleNewModal() {
+  	this.showNewModal = !this.showNewModal;
+  }
 
-
+  postSwap() {
+  	// add in posting tags!!!
+    this.http.post('http://localhost:9393/swaps/?token=' + window.localStorage.token, this.newSwap).subscribe(response => {
+      // update page with all swaps
+    }, err => {
+      alert("error");
+    })
+    this.toggleNewModal()
+  }
 }
