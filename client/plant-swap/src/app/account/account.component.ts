fb8249e300e5a692b65ba1a79cf1bd775ac40485
Jackson Tenclay <jptenclay@gmail.com>
2017-07-26 14:01:23 -0500

    account shows only own swaps

--- client/plant-swap/src/app/account/account.component.ts
+++ client/plant-swap/src/app/account/account.component.ts
@@ -23,10 +23,25 @@ export class AccountComponent {
 
 	showNewModal: boolean = false;
 	newSwap: Swap = new Swap();
+	baseUrl = 'http://localhost:9393/';
+	swaps: Swap[] = [];
 
   constructor(private router: Router, private dataService: DataService, private http: Http) {
   	this.newSwap.user_id = window.localStorage.id;
   	this.newSwap.is_open = true;
+  	this.getSwaps();
+  }
+
+  getSwaps() {
+		this.http.get(this.baseUrl + 'swaps?token=' + window.localStorage.token).subscribe(response => {
+			for (let swap of response.json().swaps) {
+				if (swap.user.id == window.localStorage.id) {
+					this.swaps.push(swap)
+				}
+			}
+    }, err => {
+      alert("error");
+    })
   }
 
   logout($event) {
@@ -43,7 +58,7 @@ export class AccountComponent {
 
   postSwap() {
   	// add in posting tags!!!
-    this.http.post('http://localhost:9393/swaps/?token=' + window.localStorage.token, this.newSwap).subscribe(response => {
+    this.http.post(this.baseUrl + 'swaps/?token=' + window.localStorage.token, this.newSwap).subscribe(response => {
       // update page with all swaps
     }, err => {
       alert("error");
